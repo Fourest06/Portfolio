52044ca7882c015898d88b3e2efd067141b60ed9
Fourest <fourestmalinao6@gmail.com>
2024-12-01 11:48:05 +0800

    Slight change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
 
                 <div className='md:block lg:hidden'>
                     <a
-                      href='/Fourest_Glenn_Malinao_Resume.pdf' 
+                      href='/public/Fourest_Glenn_Malinao_Resume.pdf' 
                       download='Fourest_Glenn_Malinao_Resume.pdf' 
                       className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                         Resume
